ccaab07427a27e685f2240df1b379c4ca8ab7918
mayerbryan <mayerbryan@mail.com>
2022-07-07 17:22:19 -0300

    initial creation of user database through ef migrations

---
+++ ProjectMarket.Api/Data/Mappings/UserClientMap.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProjectMarket.Application.Models;
+
+namespace ProjectMarket.Api.Data.Mappings
+{
+    public class UserClientMap : IEntityTypeConfiguration<UserClientModel>
+    {
+        public void Configure(EntityTypeBuilder<UserClientModel> builder)
+        {
+            builder.ToTable("UserClient");
+
+            builder.HasKey(x => x.SystemId);
+
+            builder.Property(x => x.SystemId)
+                .ValueGeneratedOnAdd();
+
+            builder.Property(x => x.UserName)
+                .IsRequired()
+                .HasColumnName("UserClientName")
+                .HasColumnType("text")
+                .HasMaxLength(50);
+
+            builder.Property(x => x.UserAddress)
+                .IsRequired()
+                .HasColumnName("UserClientAddress")
+                .HasColumnType("text")
+                .HasMaxLength(80);
+
+            builder.Property(x => x.UserPhone)
+                .IsRequired()
+                .HasColumnName("UserClientPhone")
+                .HasColumnType("integer")
+                .HasMaxLength(15);
+
+            builder.Property(x => x.UserId)
+                .IsRequired()
+                .HasColumnName("UserClientId")
+                .HasColumnType("integer")
+                .HasMaxLength(15);
+
+            builder.Property(x => x.UserEmail)
+                .IsRequired()
+                .HasColumnName("UserClientEmail")
+                .HasColumnType("text")
+                .HasMaxLength(30);
+
+            builder.Property(x => x.UserPassword)
+                .IsRequired()
+                .HasColumnName("UserClientPassword")
+                .HasColumnType("nvarchar")
+                .HasMaxLength(50);
+
+
+       
+        }
+    }
+}
---
+++ ProjectMarket.Api/Migrations/20220707200949_UserClientInitialCreation.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectMarket.Api.Migrations
+{
+    public partial class UserClientInitialCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserClient",
+                columns: table => new
+                {
+                    SystemId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserPhone = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    UserEmail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserPassword = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserClient", x => x.SystemId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserClient");
+        }
+    }
+}
